0035bc110f2cd3df3ad88e361b368dc28c8b5464
ras <rahul.a.sharma@gmail.com>
2024-12-01 00:17:30 -0500

    part1.py

---
+++ Day1/part1.py
@@ -0,0 +1,17 @@
+array1 = []
+array2 = []
+i = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        elements = line.strip().split('   ')
+        array1.append(int(elements[0]))
+        array2.append(int(elements[1]))
+
+
+sorted_array_1 = sorted(array1)
+sorted_array_2  =sorted(array2)
+
+for index, ele in enumerate(sorted_array_1):
+    i = i + abs(sorted_array_1[index] - sorted_array_2[index])
+print(i)
